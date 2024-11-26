52fe521cee44d99cd82a0d040eedefa44b8d316f
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-11-26 23:08:51 +0800

    刪除資料

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -34,8 +34,11 @@
                 <td>
                     <a class="btn btn-sm btn-primary" href="{{ route('admin.posts.edit', $post->id) }}">編輯</a>
 
-
-                    <a class="btn btn-primary btn-sm" href="#">刪除</a>
+                    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST" style="display:inline-block">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-sm btn-danger" type="submit">刪除</button>
+                    </form>
                 </td>
             </tr>
         @endforeach
